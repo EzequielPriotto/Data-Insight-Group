27d7369567235cfced1ef3bddf31c079480a24b1
EzequielPriotto <wokreuztt@gmail.com>
2022-04-04 02:56:32 -0300

    optimizacion codigo js

--- script/main.js
+++ script/main.js
@@ -35,172 +35,140 @@ const form = document.querySelector("form")
 const collator = new Intl.Collator('en');
 const selectCuerpo = document.querySelector(`#states`);
 const tabla = document.querySelector('#cabecera')
-var tipodeOrden = "name"
-
+// const check = document.querySelector('#check')
+let tipodeOrden = "name"
 form.addEventListener("change", evento => handleForm(evento))
 tabla.addEventListener("click", event => asignarOrden(event.target))
+// check.addEventListener('change', () =>{
+//     console.log('manuelita')
+// })
 
 
-const tableUpdate = (data, idTabla, condicion) => {
-    
+const tableUpdate = (data, idTabla, condicion) => {  
     
     let tablaCuerpo = document.querySelector(`#${idTabla}`);
-    tablaCuerpo.innerHTML = ''
+    tablaCuerpo.innerHTML = '';
     if(condicion === ""){
         data['results'][0]['members'].forEach((member) =>{
             let filaNueva = document.createElement('tr');
             
             filaNueva.innerHTML = `
             
-            <th scope="row" class="fila1"><a href="${member.url}" target="_blank" rel="noopener noreferrer">${member.first_name} ${
+            <td scope="row" class="fila1"><a href="${member.url}" target="_blank">${member.first_name} ${
                     member.middle_name ? member.middle_name : ""
-                } ${member.last_name} </a></th>
-                <th>${member.party}</th>
-                <th>${member.state}</th>
-                <th>${member.seniority} years</th>
-                <th>${member.votes_with_party_pct}%</th>
+                } ${member.last_name} </a></td>
+                <td>${member.party}</td>
+                <td>${member.state}</td>
+                <td>${member.seniority} years</td>
+                <td>${member.votes_with_party_pct}%</td>
                 
                 `;
                 
-                tablaCuerpo.appendChild(filaNueva)
+                tablaCuerpo.appendChild(filaNueva);
             })
         }
-        else if(condicion === "filtrado"){
+    else if(condicion === "filtrado"){
         data.forEach((member) =>{
             let filaNueva = document.createElement('tr');
             
             filaNueva.innerHTML = `
            
-            <th scope="row" class="fila1"><a href="${member.url}" target="_blank rel="noopener noreferrer" ">${member.first_name} ${
+            <td scope="row" class="fila1"><a href="${member.url}" target="_blank rel="noopener noreferrer" ">${member.first_name} ${
                 member.middle_name ? member.middle_name : ""
-            } ${member.last_name} </a></th>
-            <th>${member.party}</th>
-            <th>${member.state}</th>
-            <th>${member.seniority} years</th>
-            <th>${member.votes_with_party_pct}%</th>
+            } ${member.last_name} </a></td>
+            <td>${member.party}</td>
+            <td>${member.state}</td>
+            <td>${member.seniority} years</td>
+            <td>${member.votes_with_party_pct}%</td>
             
             `;
             
             
-            tablaCuerpo.appendChild(filaNueva)
+            tablaCuerpo.appendChild(filaNueva);
         })
     }
     
         
 }
-tableUpdate(data, 'table-body', "")
-
+tableUpdate(data, 'table-body', "");
 
 
 const optionUpdate = (data, idSelect, condicion) =>{
-   let auxiliar = []
+   let auxiliar = [];
    if(condicion === "inicio"){
        data.results[0].members.forEach( member => {
            if(!auxiliar.includes(member.state)){
-               auxiliar.push(member.state)
-            
-        }
-    });
-    auxiliar.sort()
+               auxiliar.push(member.state);      
+        }});
+    auxiliar.sort();
     // console.log(auxiliar)
     auxiliar.forEach(state => {
         let filaNueva = document.createElement('option');
-        filaNueva.setAttribute("value",state)
+        filaNueva.setAttribute("value",state);
         // filaNueva.value = state;           OTRA MANERA DE ESCRIBIRLO
-        filaNueva.innerHTML = `${state}`
-        selectCuerpo.appendChild(filaNueva)
+        filaNueva.innerHTML = `${state}`;
+        selectCuerpo.appendChild(filaNueva);
     })
 }
    else if(condicion === "data"){
     data.results[0].members.forEach( member => {
         if(!auxiliar.includes(member.state)){
-            auxiliar.push(member.state)
-            
-        }
-    });
-    auxiliar.sort()
+            auxiliar.push(member.state)         
+        }});
+    auxiliar.sort();
     // console.log(auxiliar)
     
 }
 return auxiliar
 
 }
-optionUpdate(data, 'states', "inicio") 
+optionUpdate(data, 'states', "inicio"); 
 
 const handleForm = () =>{
-
-
-    
     // FILTRO POR SELECT (ESTADOS)
     
     let select = document.querySelector('select');
+
     let primerLista = filtrar(data, select.value, "estados");
-    // console.log(primerLista);
       
     // FILTRO POR CHECKBOXES (PARTIDOS)
-    
+
     // Capturar checkboxes
-    let checkboxes = form.querySelectorAll("input[type='checkbox']")
-    // Filtra los seleccionados. Primero debemos convertir el NodeList a array para poder usar .filter()
-    let arrayCheckboxes = Array.from(checkboxes)    
-    let checkboxesSeleccionados = arrayCheckboxes.filter(checkbox => checkbox.checked)
-    // Obtener los valores de los inputs seleccionados utilizando .map()
-    let valoresSeleccionados = checkboxesSeleccionados.map(checkbox => checkbox.value)
-    if(valoresSeleccionados.length === 0){
-        valoresSeleccionados.push("")
-    }
-    // console.log(valoresSeleccionados)
+    let checkboxes = form.querySelectorAll("input[type='checkbox']");
+    let valoresSeleccionados = Array.from(checkboxes).filter(checkbox => checkbox.checked).map(checkbox => checkbox.value)  
+    valoresSeleccionados.length === 0 ?  valoresSeleccionados.push("all") : ""
+
     let segundaLista = filtrar(primerLista, valoresSeleccionados, "partidos");
-    
-    tableUpdate(segundaLista, 'table-body', "filtrado")
-
-
+    tableUpdate(segundaLista, 'table-body', "filtrado");
 }
 
-
 const filtrar = (array, condicion, tipoFiltro) => {    
+   
     if(tipoFiltro === "estados"){
         let states = optionUpdate(data, 'states', "data");
-        let auxiliar = []
+        let auxiliar = [];
+       
         states.forEach(state => {
             if(state === condicion){
-                auxiliar = array.results[0].members.filter((miembro) =>{
-                    return miembro.state === condicion;
-                  
-                })
-            }
+                auxiliar = array.results[0].members.filter((miembro) =>miembro.state === condicion);
+            }    
             else if(condicion === "all"){
-                auxiliar =  array.results[0].members.filter(miembro => miembro)
-                
-            }
-            
-        });
+                auxiliar =  array.results[0].members.filter(miembro => miembro);
+            }});
         return auxiliar
     }
     
     else if (tipoFiltro === "partidos"){
         let auxiliar = [];
         let listaDeCheck = condicion;
-        let listaFiltrada1 = array;
-        
+        let listaFiltrada1 = array;   
         listaFiltrada1.forEach(miembro => 
-            listaDeCheck.forEach((opcion) => 
-            array.map((miembro) =>
-            miembro.party === opcion ? 
-            auxiliar.push(miembro) : opcion === "" ?
-            auxiliar = listaFiltrada1 : "")))
-            
-            let auxiliar2 =  new Set(auxiliar);
-            auxiliar = Array.from(auxiliar2)
-            var auxiliarOrdenado = auxiliar.sort(SortArray);
-            
-            // console.log(auxiliarOrdenado);
-            
-            return auxiliarOrdenado
-        }
-        
+            listaDeCheck.forEach(opcion =>
+                 miembro.party === opcion && !auxiliar.includes(miembro) ? auxiliar.push(miembro) : 
+                 opcion === "all" ? auxiliar = listaFiltrada1 : ""))                                
+        return auxiliar.sort(SortArray);           
+    }      
 }
-
 
 const asignarOrden = (tipoComparacion) => {
     Array.from(document.getElementsByClassName("THactivado")).forEach(th => th.classList.remove("THactivado"))
@@ -214,9 +182,9 @@ const asignarOrden = (tipoComparacion) => {
         tipodeOrden = "party"
         handleForm()
     }
-    else if(tipoComparacion.id === "states"){
+    else if(tipoComparacion.id === "state"){
         tipoComparacion.classList.add("THactivado")
-        tipodeOrden = "states"
+        tipodeOrden = "state"
         handleForm()
     }
     else if(tipoComparacion.id === "seniority"){
@@ -228,32 +196,23 @@ const asignarOrden = (tipoComparacion) => {
         tipoComparacion.classList.add("THactivado")
         tipodeOrden = "votes"
         handleForm()
-    }
-    
+    }  
 }
-
 
 function SortArray(x, y){
     if(tipodeOrden === "name"){
-       console.log()
         return collator.compare(x.last_name, y.last_name);
-
     }   
     else if(tipodeOrden === "party"){
         return collator.compare(x.party, y.party);
-
     }   
-    else if(tipodeOrden === "states"){
+    else if(tipodeOrden === "state"){
         return collator.compare(x.state, y.state);
-
     }   
     else if(tipodeOrden === "seniority"){
-
-        return collator.compare(y.seniority, x.seniority);
-
+        return y.seniority - x.seniority
     }   
     else if(tipodeOrden === "votes"){
-        return collator.compare(y.votes_with_party_pct, x.votes_with_party_pct);
-
+        return y.votes_with_party_pct - x.votes_with_party_pct ;
     }   
 }
